b40c440454529704f080b7a5c637b3496687db45
Wisdom Ogwu <iammadab@gmail.com>
2024-03-29 13:24:40 +0100

    turn off trailing 1s

--- src/turn_off_trailing_1s.rs
+++ src/turn_off_trailing_1s.rs
@@ -1,7 +1,13 @@
 #[cfg(test)]
 mod tests {
+    use crate::u32_bit;
+
     #[test]
     fn test_statement() {
-        todo!()
+        let v = u32_bit("1010111");
+        assert_eq!(
+            v & (v + 1),
+            u32_bit("1010000")
+        );
     }
 }
